683ab475763e3c0e3935f07a2d9744045fb31e49
0mpang1 <seoyoun059@gmail.com>
2021-09-10 22:48:22 +0900

    Update main.cpp file

--- main.cpp
+++ main.cpp
@@ -1,7 +1,6 @@
 // main.cpp
 #include <stdio.h>
 #include <stdint.h>
-#include "sum.h"
 #include "btol.h"
 
 int main(int argc, char* argv[]) {
@@ -13,7 +12,7 @@ int main(int argc, char* argv[]) {
 	fread(&b, sizeof(uint32_t),1, f2);
 	a1 = btol(a);
 	b1 = btol(b);
-	s = sum(a1, b1);
+	s = a1 + b1;
 	printf("%d(%#x) + %d(%#x) = %d(%#x)\n", a1, a1, b1, b1, s, s);
 	fclose(f1);
 	fclose(f2);
